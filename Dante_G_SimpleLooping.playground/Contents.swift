918c4e44f0147d95adf80e3552050e0de700caf7
DanteGivens <67976030+DanteGivens@users.noreply.github.com>
2020-07-14 17:42:14 -0400

    Add files via upload

---
+++ Dante_G_SimpleLooping.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var NumberinSong = 1
+//number of ants in song.
+while(NumberinSong <= 10){
+    print("The ants go marching \(NumberinSong) by \(NumberinSong) Hoorah! Hoorah! \n" + "The ants go marching \(NumberinSong) by \(NumberinSong) Hoorah! Hoorah!\n" + "The ants go marching \( NumberinSong) by\(NumberinSong), \n" + "The little one stops to suck his thumb. And they all go marching down to the ground,\n", "To get out of the rain. Boom, boom, boom, boom! \n ")
+    NumberinSong += 1
+}
